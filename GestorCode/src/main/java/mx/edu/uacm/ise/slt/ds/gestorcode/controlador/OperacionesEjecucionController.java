bf1fc0dfa5637ce88a72a2941664d01bae7ed976
Michell Aguilar <galilea.aguilar@estudiante.uacm.edu.mx>
2026-05-20 14:54:25 -0600

    creacion de mi rama, se adjuntan las pantallas que me tocaron

---
+++ GestorCode/src/main/java/mx/edu/uacm/ise/slt/ds/gestorcode/controlador/OperacionesEjecucionController.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mx.edu.uacm.ise.slt.ds.gestorcode.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author DELL VPRO
+ */
+public class OperacionesEjecucionController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }   
+    
+    
+}
